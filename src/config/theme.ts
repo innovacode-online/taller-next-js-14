0ae8539d0b9971f29af34a9aaa80998b93989899
Ale <duartemattias4@gmail.com>
2024-04-06 10:03:32 -0400

    initial config

---
+++ src/config/theme.ts
@@ -0,0 +1,33 @@
+import { ConfigThemes } from "@nextui-org/react";
+
+
+export const lightTheme: ConfigThemes = {
+    light: {
+        layout:{
+            radius: {
+                small: '5px',
+                medium: '7px',
+                large: '10px',
+            },
+            
+        },
+
+        colors: {
+            background: '#fafafb',
+            primary: {
+                DEFAULT: '#fe265c',
+                '50': '#fff0f2',
+                '100': '#ffe2e7',
+                '200': '#ffcad5',
+                '300': '#ff9fb3',
+                '400': '#ff698c',
+                '500': '#fe265c',
+                '600': '#ec1253',
+                '700': '#c80846',
+                '800': '#a70a41',
+                '900': '#8f0c3e',
+            },
+            
+        }
+    }
+}
