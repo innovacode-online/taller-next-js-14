747e00c03f8349193a6a3b2ab3c4d808cec2efec
Ale <duartemattias4@gmail.com>
2024-04-13 10:28:03 -0400

    refactor code

--- src/modules/categories/components/CategoryCard.tsx
+++ src/modules/categories/components/CategoryCard.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 export const CategoryCard = () => {
     return (
         <div className='category__card'>
-            <img src="" alt="Categoria" />
+            <img className='category__card--image' src="https://images.pexels.com/photos/2133989/pexels-photo-2133989.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="Categoria" />
 
             <h3>Categoria</h3>
         </div>
--- src/modules/products/components/ProductCard.tsx
+++ src/modules/products/components/ProductCard.tsx
@@ -3,9 +3,11 @@ import React from 'react'
 export const ProductCard = () => {
     return (
         <div className='product__card'>
-            <div className='mb-4 rounded-xl overflow-hidden'>
-                <img src="https://images.pexels.com/photos/2702674/pexels-photo-2702674.jpeg?auto=compress&cs=tinysrgb&w=600" alt="Producto" />
-            </div>
+            {/* <div className='mb-4 rounded-xl overflow-hidden'> */}
+                <img 
+                    className='product__card--image'
+                src="https://images.pexels.com/photos/2702674/pexels-photo-2702674.jpeg?auto=compress&cs=tinysrgb&w=600" alt="Producto" />
+            {/* </div> */}
             
             <h3 className='mb-2 text-2xl text-center text-gray-700'>Nombre del producto</h3>
             <p className='text-center text-xl text-gray-500'>$65.2</p>
